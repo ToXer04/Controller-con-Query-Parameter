dc1bbb3808bff5a405a7896f24e23772c424f9f7
ToXer04 <84339271+ToXer04@users.noreply.github.com>
2024-01-17 15:48:15 +0100

    Update ControllerNomeProvincia.java

--- demo-webapp/src/main/java/com/example/demowebapp/ControllerNomeProvincia.java
+++ demo-webapp/src/main/java/com/example/demowebapp/ControllerNomeProvincia.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1")
 public class ControllerNomeProvincia {
     @GetMapping("/ciao")
-    public String helloNomeProvincia(@RequestParam String nome, String provincia) {
+    public String helloNomeProvincia(
+        @RequestParam String nome, 
+        @RequestParam String provincia
+    ) {
         return "Ciao " + nome + ", com'è il tempo in " + provincia + "?";
     }
 }
